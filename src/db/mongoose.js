2a2898350808abbe8f11eb386c7fb92e439a2f5b
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-15 23:52:20 -0400

    configure index.js and database connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1.27017/fingerz-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/fingerz-api', {
   useNewUrlParser: true,
-  useCreateIndex: true
-});
+  useCreateIndex: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Database Connected'))
+  .catch(error => console.log(error));
