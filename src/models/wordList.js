52b829159e21c5907b3d8384b47089c1984752e3
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-16 12:57:31 -0400

    create WordList model

---
+++ src/models/wordList.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const WordList = mongoose.model('WordList', {
+  category: {
+    type: String,
+    required: true
+  },
+  words: {
+    type: Array,
+    required: true
+  }
+});
+
+module.exports = WordList;
