0ca8b6ed2e7053d73d2021466f2d085b54d2a5cb
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-10-17 00:12:35 -0400

    iinstall and configure cors package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const wordListRouter = require('./routers/wordList');
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(wordListRouter);
-
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
