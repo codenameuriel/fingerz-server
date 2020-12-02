4945500f274905c52fc02fbddb7a9d2d9793ae79
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-12-01 22:22:11 -0500

    setup environment variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('./db/mongoose');
 const wordListRouter = require('./routers/wordList');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
